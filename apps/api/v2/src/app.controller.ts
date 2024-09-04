d340d2956113f8dc8088462d0a17d0634d4e97b4
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-03 23:34:23 -0300

    test

--- apps/api/v2/src/app.controller.ts
+++ apps/api/v2/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Version, VERSION_NEUTRAL } from "@nestjs/common";
+import { ApiTags as DocsTags, ApiExcludeController as DocsExcludeController } from "@nestjs/swagger";
+import { getEnv } from "src/env";
 
 @Controller()
+@DocsTags("Health - development only")
+@DocsExcludeController(getEnv("NODE_ENV") === "production")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("health")
+  @Version(VERSION_NEUTRAL)
+  getHealth(): "OK" {
+    return "OK";
   }
 }
