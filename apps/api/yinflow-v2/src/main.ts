8a96cccf7c5c9bb07025825773f7d0501a7efce3
Felippe Fernandes <77205146+LFelippeDev@users.noreply.github.com>
2025-04-02 10:45:27 -0300

    Update main.ts

--- apps/api/yinflow-v2/src/main.ts
+++ apps/api/yinflow-v2/src/main.ts
@@ -1,32 +1,10 @@
-import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import type { NestExpressApplication } from "@nestjs/platform-express";
 import "dotenv/config";
 
-import { bootstrap } from "./app";
 import { AppModule } from "./app.module";
 
-const run = async () => {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    bodyParser: false,
-  });
-
-  const logger = new Logger("App");
-
-  try {
-    bootstrap(app);
-    const port = 4000;
-    await app.listen(port);
-    logger.log(`Application started on port: ${port}`);
-  } catch (error) {
-    console.error(error);
-    logger.error("Application crashed", {
-      error,
-    });
-  }
-};
-
-run().catch((error: Error) => {
-  console.error("Failed to start Cal Platform API", { error: error.stack });
-  process.exit(1);
-});
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000);
+}
+bootstrap();
