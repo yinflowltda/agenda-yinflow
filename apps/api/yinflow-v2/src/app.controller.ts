2e14d61f7c21617e4a28c0f1bb9730d6a5bbd807
Felippe Fernandes <77205146+LFelippeDev@users.noreply.github.com>
2025-04-04 17:35:30 -0300

    Update app.controller.ts

--- apps/api/yinflow-v2/src/app.controller.ts
+++ apps/api/yinflow-v2/src/app.controller.ts
@@ -1,22 +1,15 @@
 import {
-  Body,
   Controller,
   Get,
   HttpException,
   InternalServerErrorException,
-  NotFoundException,
-  Post,
+  Query,
   Req,
   Version,
   VERSION_NEUTRAL,
 } from "@nestjs/common";
-import dayjs from "dayjs";
 
-import { SUCCESS_STATUS } from "@calcom/platform-constants";
-import { BookingResponse, HttpError } from "@calcom/platform-libraries";
-import { ApiResponse, CreateBookingInput } from "@calcom/platform-types";
-
-import { supabase } from "./config/supabase";
+import { HttpError } from "@calcom/platform-libraries";
 
 type BookingRequest = Request & {
   userId?: number;
@@ -40,79 +33,132 @@ export class AppController {
     });
   }
 
-  @Get("/v2/me")
+  @Get("/v2/event-types")
   @Version(VERSION_NEUTRAL)
-  async getMe() {
+  async getEventTypes(
+    @Req() req: Request,
+    @Query("username") username: string,
+    @Query("usernames") usernames: string[],
+    @Query("eventSlug") eventSlug: string,
+    @Query("orgId") orgId: string,
+    @Query("orgSlug") orgSlug: string
+  ) {
+    const apiKey = req.headers.get("apiKey") as string;
+
+    let customParams = [];
+
+    switch (true) {
+      case !!username:
+        customParams.push(`username=${username}`);
+      case !!usernames:
+        customParams.push(`usernames=${usernames}`);
+      case !!eventSlug:
+        customParams.push(`eventSlug=${eventSlug}`);
+      case !!orgId:
+        customParams.push(`orgId=${orgId}`);
+      case !!orgSlug:
+        customParams.push(`orgSlug=${orgSlug}`);
+    }
+
+    const params = customParams.length ? `?${customParams.join("&")}` : "";
+
     try {
-      const response = await fetch(`${AGENDA_BASE_URL}/yinflow-me`, {
+      const response = await fetch(`${AGENDA_BASE_URL}/yinflow-get-event-types${params}`, {
+        headers: {
+          apiKey,
+        },
         method: "GET",
       });
 
       if (!response.ok) throw new HttpException(response.statusText, response.status);
 
       return await response.json();
-
     } catch (err) {
       const error = err as Error;
       throw new InternalServerErrorException(error?.message);
     }
-    throw new InternalServerErrorException("Could not get me.");
   }
 
-  @Post("/v2/bookings")
-  async createBooking(
-    @Req() req: BookingRequest,
-    @Body() body: CreateBookingInput
-  ): Promise<ApiResponse<Partial<BookingResponse>>> {
-    const { start, metadata, lengthInMinutes } = body;
+  // @Get("/v2/me")
+  // @Version(VERSION_NEUTRAL)
+  // async getMe(
+  //   @Req() req: Request,
+  //   @Query('id') id: string
+  // ) {
+  //   const idParam = id ? `?id=${id}` : "";
 
-    const end = dayjs(start)
-      .add(lengthInMinutes || 50, "minutes")
-      .toISOString();
+  //   try {
+  //     const response = await fetch(`${AGENDA_BASE_URL}/yinflow-me${idParam}`, {
+  //       headers:{
+  //         apiKey: req.headers.apikey as string || "",
+  //       }
+  //       method: "GET",
+  //     });
 
-    try {
-      const { data: eventType } = await supabase
-        .from("EventType")
-        .select("title")
-        .eq("id", req.body.eventTypeId)
-        .single();
+  //     if (!response.ok) throw new HttpException(response.statusText, response.status);
 
-      if (!eventType) throw new NotFoundException("Event type not found.");
+  //     return await response.json();
+  //   } catch (err) {
+  //     const error = err as Error;
+  //     throw new InternalServerErrorException(error?.message);
+  //   }
+  //   throw new InternalServerErrorException("Could not get me.");
+  // }
 
-      try {
-        const response = await fetch(`${AGENDA_BASE_URL}/yinflow-post-booking`, {
-          body: JSON.stringify({
-            eventTypeId: req.body.eventTypeId,
-            start: req.body.start,
-            end,
-            responses: req.body.bookingFieldsResponses || {},
-            metadata: metadata || {},
-            timeZone: req.body.attendee.timeZone,
-            language: req.body.attendee.language,
-            title: eventType.title,
-          }),
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+  // @Post("/v2/bookings")
+  // async createBooking(
+  //   @Req() req: BookingRequest,
+  //   @Body() body: CreateBookingInput
+  // ): Promise<ApiResponse<Partial<BookingResponse>>> {
+  //   const { start, metadata, lengthInMinutes } = body;
 
-        if (!response.ok) throw new HttpException(response.statusText, response.status);
+  //   const end = dayjs(start)
+  //     .add(lengthInMinutes || 50, "minutes")
+  //     .toISOString();
 
-        const responseData = await response.json();
+  //   try {
+  //     const { data: eventType } = await supabase
+  //       .from("EventType")
+  //       .select("title")
+  //       .eq("id", req.body.eventTypeId)
+  //       .single();
 
-        return {
-          status: SUCCESS_STATUS,
-          data: responseData,
-        };
-      } catch (err) {
-        this.handleBookingErrors(err);
-      }
-    } catch (err) {
-      this.handleBookingErrors(err);
-    }
-    throw new InternalServerErrorException("Could not create booking.");
-  }
+  //     if (!eventType) throw new NotFoundException("Event type not found.");
+
+  //     try {
+  //       const response = await fetch(`${AGENDA_BASE_URL}/yinflow-post-booking`, {
+  //         body: JSON.stringify({
+  //           eventTypeId: req.body.eventTypeId,
+  //           start: req.body.start,
+  //           end,
+  //           responses: req.body.bookingFieldsResponses || {},
+  //           metadata: metadata || {},
+  //           timeZone: req.body.attendee.timeZone,
+  //           language: req.body.attendee.language,
+  //           title: eventType.title,
+  //         }),
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       });
+
+  //       if (!response.ok) throw new HttpException(response.statusText, response.status);
+
+  //       const responseData = await response.json();
+
+  //       return {
+  //         status: SUCCESS_STATUS,
+  //         data: responseData,
+  //       };
+  //     } catch (err) {
+  //       this.handleBookingErrors(err);
+  //     }
+  //   } catch (err) {
+  //     this.handleBookingErrors(err);
+  //   }
+  //   throw new InternalServerErrorException("Could not create booking.");
+  // }
 
   private handleBookingErrors(
     err: Error | HttpError | unknown,
